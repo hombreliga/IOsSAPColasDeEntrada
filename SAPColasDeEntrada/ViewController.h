cf9e0fed73d427ab914ad08fbf55856b0a570701
Cesar Yañez <cesaryanez@Mac-mini-de-Cesar.local>
2014-09-02 11:29:00 -0500

    Initial Commit

---
+++ SAPColasDeEntrada/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SAPColasDeEntrada
+//
+//  Created by Cesar Yañez on 02/09/14.
+//  Copyright (c) 2014 Omnilife. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ SAPColasDeEntrada/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SAPColasDeEntrada
+//
+//  Created by Cesar Yañez on 02/09/14.
+//  Copyright (c) 2014 Omnilife. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
